0567f90ec465b6401a24cabb5ed6a73f22f324b6
farm prakasit <farmconom@gmail.com>
2024-10-19 14:03:04 +0700

    update

--- js/contact-submit-buttuon.js
+++ js/contact-submit-buttuon.js
@@ -1,13 +1,13 @@
 const sendBtn = document.getElementById('send-button'),
-      loader = document.getElementById('loader'),
-      textarea = document.getElementById('hold-textarea');
-      
+    loader = document.getElementById('loader'),
+    textarea = document.getElementById('messageInput');
+
 
 
 sendBtn.addEventListener('click', () => {
     document.getElementById('real-button').click();
-    loader.style.display = 'inline-block'; 
-    
+    loader.style.display = 'inline-block';
+
     loader.classList.remove('loader');
     loader.offsetHeight;
     loader.classList.add('loader');
@@ -19,10 +19,24 @@ textarea.addEventListener('click', () => {
     textarea.style.height = '100px';
 });
 
-function addHyphen() {
-    var input = document.getElementById('emailInput');
+function sendEmail(event) {
+    event.preventDefault(); // Prevent the form from submitting
 
-    if (input.value != "") {
-        input.value = input.value + "`";
+    const nameInput = document.getElementById('nameInput').value;
+    const subjectInput = document.getElementById('subjectInput').value;
+    const messageInput = document.getElementById('messageInput').value;
+
+    if (nameInput !== "") {
+        const mailtoLink = `mailto:${'prakasit.lertprakitsin@gmail.com'}?subject=${encodeURIComponent(subjectInput)}&body=${encodeURIComponent("Hi, my name is " + nameInput + "\n\n" + messageInput)}`;
+
+        const emailAnchor = document.getElementById('emailAnchor');
+
+        if (emailAnchor) {
+            emailAnchor.href = mailtoLink;
+
+            emailAnchor.click();
+        }
+    } else {
+        alert("Please enter a valid email address.");
     }
-  }
+}
